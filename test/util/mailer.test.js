e424c45b129d1615d09d86362f5a5bd18ae39214
ThomasKiec <kiechle_t@outlook.com>
2019-08-13 23:43:17 +0200

    Add unti test for mailer

--- test/util/mailer.test.js
+++ test/util/mailer.test.js
@@ -1,29 +1,133 @@
+import { sendPasswordResetEmail, sendSignupEmail } from '../../src/util/mailer'
 import { createTransport } from 'nodemailer'
-import { isTSAnyKeyword } from '@babel/types'
-import { sendSignupEmail } from '../../src/util/mailer'
 
-const transporter = {
-    createTransport: jest.fn(() => ({ sendMail: jest.fn() }))
-}
-jest.mock('nodemailer', transporter)
+jest.mock('nodemailer')
 
 describe('mailer', () => {
-    const mailer = createTransport.mock.results[]
+  const email = 'email'
+  const password = 'password'
 
-    console.log(mailer)
+  const emailPassword = 'emailPassword'
+  const emailUser = 'emailUser'
 
-    const email = 'email'
-    const password = 'password'
+  const sendMail = jest.fn()
 
-    const info = Symbol('info')
+  beforeAll(() => {
+    createTransport.mockImplementation(() => ({ sendMail }))
 
-    afterEach(() => {
-        mailer.sendMail.mockReset()
+    process.env.EMAIL_PASSWORD = emailPassword
+    process.env.EMAIL_USER = emailUser
+  })
+
+  afterEach(() => {
+    createTransport.mockClear()
+    sendMail.mockReset()
+  })
+
+  afterAll(() => {
+    delete process.env.EMAIL_PASSWORD
+    delete process.env.EMAIL_USER
+  })
+
+  describe('sendSignupEmail', () => {
+    it('sends e-mail and resolves info if email and password are given', async () => {
+      const test = Symbol('info')
+      sendMail.mockResolvedValueOnce(test)
+
+      await expect(sendSignupEmail(email, password)).resolves.toEqual(test)
+
+      expect(createTransport).toHaveBeenCalledTimes(1)
+      expect(createTransport).toHaveBeenCalledWith({
+        auth: { pass: emailPassword, user: emailUser },
+        service: 'Gmail',
+      })
+
+      expect(sendMail).toHaveBeenCalledTimes(1)
     })
 
-    it('sends mail if email and password are given', async () => {
-        mailer.mockResolvedValueOnce(info)
+    it('throws error if createTransport fails', async () => {
+      const error = new Error(' ¯\\_(ツ)_/¯')
 
-        await expect(sendSignupEmail(email, password)).resolves.toBe(info)
+      createTransport.mockImplementationOnce(() => {
+        throw error
+      })
+
+      await expect(sendSignupEmail(email, password)).rejects.toEqual(error)
+
+      expect(createTransport).toHaveBeenCalledTimes(1)
+      expect(createTransport).toHaveBeenCalledWith({
+        auth: { pass: emailPassword, user: emailUser },
+        service: 'Gmail',
+      })
+
+      expect(sendMail).not.toHaveBeenCalled()
     })
+
+    it('throws error if sendMail fails', async () => {
+      const error = new Error(' ¯\\_(ツ)_/¯')
+
+      sendMail.mockRejectedValueOnce(error)
+
+      await expect(sendSignupEmail(email, password)).rejects.toEqual(error)
+
+      expect(createTransport).toHaveBeenCalledTimes(1)
+      expect(createTransport).toHaveBeenCalledWith({
+        auth: { pass: emailPassword, user: emailUser },
+        service: 'Gmail',
+      })
+
+      expect(sendMail).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  describe('sendPasswordResetEmail', () => {
+    it('sends e-mail and resolves info if email and password are given', async () => {
+      const test = Symbol('info')
+      sendMail.mockResolvedValueOnce(test)
+
+      await expect(sendPasswordResetEmail(email, password)).resolves.toEqual(test)
+
+      expect(createTransport).toHaveBeenCalledTimes(1)
+      expect(createTransport).toHaveBeenCalledWith({
+        auth: { pass: emailPassword, user: emailUser },
+        service: 'Gmail',
+      })
+
+      expect(sendMail).toHaveBeenCalledTimes(1)
+    })
+
+    it('throws error if createTransport fails', async () => {
+      const error = new Error(' ¯\\_(ツ)_/¯')
+
+      createTransport.mockImplementationOnce(() => {
+        throw error
+      })
+
+      await expect(sendPasswordResetEmail(email, password)).rejects.toEqual(error)
+
+      expect(createTransport).toHaveBeenCalledTimes(1)
+      expect(createTransport).toHaveBeenCalledWith({
+        auth: { pass: emailPassword, user: emailUser },
+        service: 'Gmail',
+      })
+
+      expect(sendMail).not.toHaveBeenCalled()
+    })
+
+    it('throws error if sendMail fails', async () => {
+      const error = new Error(' ¯\\_(ツ)_/¯')
+
+      sendMail.mockRejectedValueOnce(error)
+
+      await expect(sendPasswordResetEmail(email, password)).rejects.toEqual(error)
+
+      expect(createTransport).toHaveBeenCalledTimes(1)
+      expect(createTransport).toHaveBeenCalledWith({
+        auth: { pass: emailPassword, user: emailUser },
+        service: 'Gmail',
+      })
+
+      expect(sendMail).toHaveBeenCalledTimes(1)
+    })
+  })
 })
